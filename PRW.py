bb11e733cc7ac2d56096fb28adb3f996742bb161
K.R.Schuur <57346737+KRS-dev@users.noreply.github.com>
2020-04-24 08:27:13 +0200

    Update PRW.py

--- PRW.py
+++ PRW.py
@@ -406,7 +406,7 @@ class PRW_Data_Opvrager:
                 'name':             'Grondwaterstand in mNAP',
                 'name_font':        {'size': 14, 'bold': True},
                 'major_gridlines':  {'visible': True},
-                'crossing':         minGWS
+                'crossing':         minGWS//1
             })
             chart.set_size({'x_scale': 2, 'y_scale': 1.5})
             chart.set_legend({'font': {'size': 12, 'bold': True}})
