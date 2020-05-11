c13921d1156c340090c0254066f0f55624a7027d
K.R.Schuur <57346737+KRS-dev@users.noreply.github.com>
2020-05-11 12:13:12 +0200

    fix shapefile out

--- PRW.py
+++ PRW.py
@@ -725,7 +725,7 @@ class HeavyLifting(QgsTask):
             df_pbs = df_pbs.sort_values(by='PEILBUIS')
             df_pbStats = df_pbStats.sort_values(by='PEILBUIS')
 
-            df_pbs_shp = pd.concat([df_pbs, df_pbStats.loc[:, 'Maximaal gemeten':'Minimaal gemeten']], axis=0)
+            df_pbs_shp = pd.concat([df_pbs, df_pbStats.loc[:, 'Maximaal gemeten':'Minimaal gemeten']], axis=1, ignore_index=True)
             for i in df_pbs_shp:
                 if df_pbs_shp[i].dtypes == 'datetime64[ns]':
                     df_pbs_shp[i] = df_pbs_shp.astype(str)
